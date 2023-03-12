ab7cc8ee943054a5d22ad9edacad194580398d0e
polidex <lp.igor@gmail.com>
2023-03-12 19:54:39 -0300

    feat: reqt 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,3 +1,4 @@
+import json
 import csv
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -5,8 +6,12 @@ from inventory_report.reports.complete_report import CompleteReport
 
 class Inventory():
     def import_data(path, type):
-        with open(path) as file:
-            list = [item for item in csv.DictReader(file)]
+        if (path.__contains__('csv')):
+            with open(path) as file:
+                list = [item for item in csv.DictReader(file)]
+        elif (path.__contains__('json')):
+            with open(path) as file:
+                list = json.load(file)
 
         if type == 'simples':
             return SimpleReport.generate(list)
