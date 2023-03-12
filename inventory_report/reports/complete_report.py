962f8cf5c3f7ca748194fa042c874d4ff40680f7
polidex <lp.igor@gmail.com>
2023-03-12 19:20:01 -0300

    feat: reqt 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,14 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, products):
+        data = super().generate(products)
+        company = Counter(row['nome_da_empresa'] for row in products)
+        stock = ''
+        for key, value in company.items():
+            stock += f'- {key}: {value}\n'
+        return (data + '\n' + 'Produtos estocados por empresa:\n{}'
+                ).format(stock)
