859eb934dce9ca6e96682d52e67d85b86517d7e4
polidex <lp.igor@gmail.com>
2023-03-12 13:57:41 -0300

    fix: linter 2.0

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,7 +2,7 @@ from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    productMock = Product(
+    product = Product(
         1,
         'CADEIRA',
         'Forces of Nature',
@@ -12,10 +12,10 @@ def test_cria_produto():
         'Conservar em local fresco'
     )
 
-    assert productMock.id == 1
-    assert productMock.nome_do_produto == 'CADEIRA'
-    assert productMock.nome_da_empresa == 'Forces of Nature'
-    assert productMock.data_de_fabricacao == '2022-04-04'
-    assert productMock.data_de_validade == '2023-02-09'
-    assert productMock.numero_de_serie == 'FR48'
-    assert productMock.instrucoes_de_armazenamento == 'Local fresco'
+    assert product.id == 1
+    assert product.nome_do_produto == 'CADEIRA'
+    assert product.nome_da_empresa == 'Forces of Nature'
+    assert product.data_de_fabricacao == '2022-04-04'
+    assert product.data_de_validade == '2023-02-09'
+    assert product.numero_de_serie == 'FR48'
+    assert product.instrucoes_de_armazenamento == 'Conservar em local fresco'
