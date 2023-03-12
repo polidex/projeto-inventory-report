cee90d7a59e496415aa87e4acb9eefaf47c3d291
polidex <lp.igor@gmail.com>
2023-03-12 13:02:29 -0300

    [ Igor Polido ] invetory report: projeto iniciado

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    productMock = Product(
+        1,
+        'CADEIRA',
+        'Forces of Nature',
+        '2022-04-04',
+        '2023-02-09',
+        'FR48',
+        'Conservar em local fresco'
+    )
+
+assert productMock.id == 1
+assert productMock.nome_do_produto == "CADEIRA"
+assert productMock.nome_da_empresa == "Forces of Nature"
+assert productMock.data_de_fabricacao == "2022-04-04"
+assert productMock.data_de_validade == "2023-02-09"
+assert productMock.numero_de_serie == "FR48"
+assert productMock.instrucoes_de_armazenamento == "Conservar em local fresco"
