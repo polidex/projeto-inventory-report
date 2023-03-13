7898c5f52677c47d25f2faf41325e083617a8365
polidex <lp.igor@gmail.com>
2023-03-13 09:00:12 -0300

    test: reqt 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'CADEIRA',
+        'Forces of Nature',
+        '2022-04-04',
+        '2023-02-09',
+        'FR48',
+        'Conservar em local fresco'
+    )
+
+    assert str(product) == (
+        'O produto CADEIRA fabricado em 2022-04-04 por Forces of Nature com '
+        'validade até 2023-02-09 precisa ser armazenado Conservar em local '
+        'fresco.'
+    )
